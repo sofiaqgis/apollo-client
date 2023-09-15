6e49122f5728eaf10a0d42a6b38730f854bd2faa
sofiaqgis <sofia_mato@hotmail.com>
2023-09-15 16:02:35 -0300

    Changes in client

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -1,4 +1,7 @@
+import type { AppProps } from "next/app";
 import { useMemo } from "react";
+import merge from "deepmerge";
+import isEqual from "lodash/isEqual";
 import {
   ApolloClient,
   HttpLink,
@@ -6,9 +9,7 @@ import {
   NormalizedCacheObject,
   ApolloProvider
 } from "@apollo/client";
-import type { AppProps } from "next/app";
-import merge from "deepmerge";
-import isEqual from "lodash/isEqual";
+import { NextPageContext } from "next";
 export const APOLLO_STATE_PROP_NAME = "__APOLLO_STATE__";
 let apolloClient: ApolloClient<NormalizedCacheObject> | undefined;
 
@@ -21,18 +22,13 @@ function createApolloClient() {
     cache: new InMemoryCache(),
   });
 }
-
 export function initializeApollo(
   initialState: NormalizedCacheObject | null = null
 ) {
   const _apolloClient = apolloClient ?? createApolloClient();
-
   if (initialState) {
-
     const existingCache = _apolloClient.extract();
-
     const data = merge(initialState, existingCache, {
-
       arrayMerge: (destinationArray: any, sourceArray: any) => [
         ...sourceArray,
         ...destinationArray.filter((d: any) =>
@@ -40,20 +36,15 @@ export function initializeApollo(
         ),
       ],
     });
-
     // Restore the cache with the merged data
     _apolloClient.cache.restore(data);
   }
-
   // For SSG and SSR always create a new Apollo Client
   if (typeof window === "undefined") return _apolloClient;
-
   // Create the Apollo Client once in the client
   if (!apolloClient) apolloClient = _apolloClient;
-
   return _apolloClient;
 }
-
 export function addApolloState(
   client: ApolloClient<NormalizedCacheObject>,
   pageProps: any
@@ -61,22 +52,36 @@ export function addApolloState(
   if (pageProps?.props) {
     pageProps.props[APOLLO_STATE_PROP_NAME] = client.cache.extract();
   }
-
   return pageProps;
 }
 
 export function useApollo(pageProps: any) {
-  const state = pageProps[APOLLO_STATE_PROP_NAME];
+  const state = pageProps? pageProps[APOLLO_STATE_PROP_NAME]: null;
+  console.log("state", state, pageProps);
   const store = useMemo(() => initializeApollo(state), [state]);
   return store;
 }
 
-export function ApolloWrapper({ Component, pageProps }: AppProps) {
+interface Props {
+  children?: any
+  pageProps: NextPageContext
+}
+
+export function ApolloWrapper({ children, pageProps }: Props) {
   const apolloClient = useApollo(pageProps)
+
 
   return (
     <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
+      {children}
     </ApolloProvider>
   );
 }
+
+
+
+
+
+
+
+
--- src/pages/detail/[id].tsx
+++ src/pages/detail/[id].tsx
@@ -1,13 +1,160 @@
-import { initializeApollo, addApolloState } from "../../app/client";
-import { gql, useMutation } from "@apollo/client";
+// import { initializeApollo, addApolloState } from "../../app/client";
+// import { gql, useMutation } from "@apollo/client";
+// import { GetStaticPropsContext } from "next";
+// import { ApolloWrapper } from "../../app/apolloClient";
+// import React, { useState } from 'react'
+// import { Img, ImgWrapper, Article } from './../../components/PhotoCard/styles'
+// import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+// import { Button } from '../../components/PhotoCard/styles'
+
+//   const QUERY_PHOTO = gql`
+//   query Photo($photoId: ID!) {
+//     photo(id: $photoId) {
+//       id
+//       categoryId
+//       src
+//       userId
+//       likes
+//     }
+//   }
+//   `
+
+//   interface PhotoCardProps {
+//     id: number;
+//     categoryId: number;
+//     src: string;
+//     userId: number;
+//     likes: number;
+//   }
+
+//   const LIKE_PHOTO = gql`
+// mutation LikePhoto($input: LikePhoto!) {
+//   likePhoto(input: $input) {
+//     code
+//     success
+//     message
+//     photo {
+//       likes
+//       id
+//     }
+//   }
+// }
+// `
+  
+//    export async function getStaticPaths() {
+//     return {
+//       paths: [], // Leave it empty since you want to generate paths dynamically
+//       fallback: "blocking", // Set fallback to "blocking" to enable dynamic static generation
+//     };
+//   }
+  
+//   export async function getStaticProps(context: GetStaticPropsContext) {
+//     const apolloClient = initializeApollo();
+  
+//     const { data } = await apolloClient.query({
+//       query: QUERY_PHOTO,
+//       variables: {
+//         photoId: context.params!.id,
+//       },
+//     });
+
+//     return addApolloState(apolloClient, {
+//       props: {
+//         id: context.params!.id,
+//         photo: data.photo, 
+//       },
+//     });
+//   }
+
+//   export function PhotoPage({photo, id}:{photo:PhotoCardProps, id:number}) {
+//     const key = `like-${id}`
+//     const [liked, setLiked] = useState(false)
+//      const Icon = liked ? MdFavorite : MdFavoriteBorder
+//      const [changeLikes] =  useMutation(LIKE_PHOTO)
+   
+//      const handleFavClick = async () => {
+//        const newAction = liked ? "unlike" : "like";
+//        try {
+//          if (!liked) {
+//            setLiked(true);
+//          } else {
+//            setLiked(false);
+//          }
+//        changeLikes({
+//          variables: {
+//            input: {
+//              id: id,
+//              action: newAction
+//            },
+//          },
+//        });
+//      } catch (err) {
+//        console.error('Error occurred while registering user:', err);
+//      }
+//      };
+
+//     if (!photo) {
+//       return <div>Loading...</div>; // Handle the case when the photo data is not available yet
+//     }
+  
+
+//     return (
+//         <Article>
+//             <a href={`/detail/${id}`}>
+//               <ImgWrapper>
+//                 <Img width='100%' src={photo.src} alt='PhotoCard' />
+//               </ImgWrapper>
+//             </a>
+//             <Button onClick={handleFavClick}>
+//             <Icon size='32px' />{photo.likes} likes!
+//             </Button>
+//       </Article>
+//     )
+//   }
+
+// function ShowPage({ photo, id }: { photo: PhotoCardProps, id: number }) {
+//     return(
+//       <ApolloWrapper>
+//     {photo && 
+//       <PhotoPage photo={photo} id={id}/>
+//  }
+//   </ApolloWrapper>
+//     )
+//   }
+
+// export default ShowPage
+
+import { initializeApollo, addApolloState, ApolloWrapper } from "../../app/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 import { GetStaticPropsContext } from "next";
-import { ApolloWrapper } from "../../app/apolloClient";
+//import { ApolloWrapper } from "../../app/apolloClient";
 import React, { useState } from 'react'
 import { Img, ImgWrapper, Article } from './../../components/PhotoCard/styles'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 import { Button } from '../../components/PhotoCard/styles'
 
-  const QUERY_PHOTO = gql`
+interface PhotoCardProps {
+  id: number;
+  categoryId: number;
+  src: string;
+  userId: number;
+  likes: number;
+}
+const LIKE_PHOTO = gql`
+mutation LikePhoto($input: LikePhoto!) {
+likePhoto(input: $input) {
+  code
+  success
+  message
+  photo {
+    likes
+    id
+  }
+}
+}
+`
+
+const QUERY_PHOTO = gql`
   query Photo($photoId: ID!) {
     photo(id: $photoId) {
       id
@@ -19,107 +166,89 @@ import { Button } from '../../components/PhotoCard/styles'
   }
   `
 
-  interface PhotoCardProps {
-    id: number;
-    categoryId: number;
-    src: string;
-    userId: number;
-    likes: number;
-  }
-
-  const LIKE_PHOTO = gql`
-mutation LikePhoto($input: LikePhoto!) {
-  likePhoto(input: $input) {
-    code
-    success
-    message
-    photo {
-      likes
-      id
-    }
-  }
+export async function getStaticPaths() {
+  return {
+    paths: [], // Leave it empty since you want to generate paths dynamically
+    fallback: "blocking", // Set fallback to "blocking" to enable dynamic static generation
+  };
 }
-`
-  
-   export async function getStaticPaths() {
-    return {
-      paths: [], // Leave it empty since you want to generate paths dynamically
-      fallback: "blocking", // Set fallback to "blocking" to enable dynamic static generation
-    };
-  }
-  
-  export async function getStaticProps(context: GetStaticPropsContext) {
-    const apolloClient = initializeApollo();
-  
-    const { data } = await apolloClient.query({
-      query: QUERY_PHOTO,
-      variables: {
-        photoId: context.params!.id,
-      },
-    });
 
-    return addApolloState(apolloClient, {
-      props: {
-        id: context.params!.id,
-        photo: data.photo, // Add the fetched photo data to the props
-      },
-    });
-  }
+export async function getStaticProps(context: GetStaticPropsContext) {
+  const apolloClient = initializeApollo();
 
-  export function PhotoPage({photo, id}:{photo:PhotoCardProps, id:number}) {
+  const { data } = await apolloClient.query({
+    query: QUERY_PHOTO,
+    variables: {
+      photoId: context.params!.id,
+    },
+  });
+  return addApolloState(apolloClient, {
+    props: {
+      id: context.params!.id,
+     photo: data.photo, // Add the fetched photo data to the props
+    },
+  });
+}
+
+export function PhotoPage({ id}:{ photo: PhotoCardProps, id:number}) {
     const key = `like-${id}`
     const [liked, setLiked] = useState(false)
      const Icon = liked ? MdFavorite : MdFavoriteBorder
      const [changeLikes] =  useMutation(LIKE_PHOTO)
-   
+
+     const { data } = useQuery(QUERY_PHOTO,{
+      variables: {
+        photoId: id,
+      }, fetchPolicy: "cache-only"
+    });
+
+     const photo = data ? data : {photo: null};
+
      const handleFavClick = async () => {
-       const newAction = liked ? "unlike" : "like";
-       try {
-         if (!liked) {
-           setLiked(true);
-         } else {
-           setLiked(false);
-         }
-       changeLikes({
-         variables: {
-           input: {
-             id: id,
-             action: newAction
-           },
-         },
-       });
-     } catch (err) {
-       console.error('Error occurred while registering user:', err);
-     }
-     };
+      const newAction = liked ? "unlike" : "like";
+      try {
+        if (!liked) {
+          setLiked(true);
+        } else {
+          setLiked(false);
+        }
+      changeLikes({
+        variables: {
+          input: {
+            id: id,
+            action: newAction
+          },
+        },
+      });
+    } catch (err) {
+      console.error('Error occurred while registering user:', err);
+    }
+    };
 
     if (!photo) {
-      return <div>Loading...</div>; // Handle the case when the photo data is not available yet
+     return <div>Loading...</div>; // Handle the case when the photo data is not available yet
     }
-  
 
     return (
-        <Article>
-            <a href={`/detail/${id}`}>
-              <ImgWrapper>
-                <Img width='100%' src={photo.src} alt='PhotoCard' />
-              </ImgWrapper>
-            </a>
-            <Button onClick={handleFavClick}>
-            <Icon size='32px' />{photo.likes} likes!
-            </Button>
-      </Article>
-    )
-  }
+      <Article>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img width='100%' src={photo.src} alt='PhotoCard' />
+            </ImgWrapper>
+          </a>
+          <Button onClick={handleFavClick}>
+          <Icon size='32px' />{photo.likes} likes!
+          </Button>
+    </Article>
+  )
+}
 
-function ShowPage({ photo, id }: { photo: PhotoCardProps, id: number }) {
-    return(
-      <ApolloWrapper>
-    {photo && 
+
+  function ShowPage({photo, id, pageProps }: {photo: PhotoCardProps, id: number, pageProps: any}) {
+      return(
+        <ApolloWrapper {...pageProps}>
       <PhotoPage photo={photo} id={id}/>
- }
   </ApolloWrapper>
-    )
-  }
-
-export default ShowPage
+        )
+      }
+    export default ShowPage
